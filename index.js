8a870de60e337160429abf62268db4683583db94
Sabongi <93168272+Sabongi@users.noreply.github.com>
2022-08-15 20:25:32 -0300

    criacao projeto
    configuração bibliotecas, index.js e nodemon

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+//const porta = process.env.PORT || 3333;
+
+const porta = 3333;
+
+// Define a porta do servidor - definir de acordo com a oferecida pelo serviço de hospedagem
+app.listen(porta, () => {
+    console.log('Servidor iniciado na porta: ' + porta);
+});
